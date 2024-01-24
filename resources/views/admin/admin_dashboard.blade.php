0541311e5bcd01482b273b3711fc5441253c474d
Musfikur-93 <iradil713@gmail.com>
2024-01-24 10:41:18 +0600

    Breeze Auth

---
+++ resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <title>Admin Dashboard</title>
+
+    <body>
+        <h1>Admin Dashboard</h1>
+    </body>
+</html>
---
+++ resources/views/instructor/instructor_dashboard.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <title>Instructor Dashboard</title>
+
+    <body>
+        <h1>Instructor Dashboard</h1>
+    </body>
+</html>
