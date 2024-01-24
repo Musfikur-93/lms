93fc65ee0168d0e81e2a3b8bd434e81f0edb41c3
Musfikur-93 <iradil713@gmail.com>
2024-01-24 11:53:28 +0600

    Auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InstructorController;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +21,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// User
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -26,6 +30,27 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+ });
 
 require __DIR__.'/auth.php';
+
+
+// Admin Group Middleware
+Route::middleware(['auth','roles:admin'])->group(function(){
+
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+}); // End Admin Group Middleware
+
+
+// Instructor Group Middleware
+Route::middleware(['auth','roles:instructor'])->group(function(){
+
+    Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+
+}); // End Instructor Group Middleware
+
+// Instructor
+
+
+
