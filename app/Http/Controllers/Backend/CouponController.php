bff21bb6142173287297374f021ededa2e70dbe4
Musfikur-93 <iradil713@gmail.com>
2024-05-12 13:04:47 +0600

    Backend Instructor Coupon Part Complete

--- app/Http/Controllers/Backend/CouponController.php
+++ app/Http/Controllers/Backend/CouponController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Coupon;
-use App\Models\SubCategory;
+use App\Models\Course;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class CouponController extends Controller
 {
@@ -84,7 +85,99 @@ class CouponController extends Controller
 
         return redirect()->back()->with($notification);
 
-    }
+    } // End Method
+
+
+    /////////////// Instructor Coupon Method /////////////
+
+    public function InstructorAllCoupon(){
+
+        $id = Auth::user()->id;
+        $coupon = Coupon::where('instructor_id',$id)->latest()->get();
+
+        return view('instructor.coupon.coupon_all',compact('coupon'));
+
+    } // End Method
+
+    public function InstructorAddCoupon(){
+
+        $id = Auth::user()->id;
+        $courses = Course::where('instructor_id',$id)->get();
+
+        return view('instructor.coupon.coupon_add',compact('courses'));
+
+    } // End Method
+
+    public function InstructorStoreCoupon(Request $request){
+
+        Coupon::insert([
+            'coupon_name' => strtoupper($request->coupon_name),
+            'coupon_discount' => $request->coupon_discount,
+            'coupon_validity' => $request->coupon_validity,
+            'instructor_id' => Auth::user()->id,
+            'course_id' => $request->course_id,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'Coupon Inserted Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->route('instructor.all.coupon')->with($notification);
+
+
+    } // End Method
+
+
+    public function InstructorEditCoupon($id){
+
+        $coupon = Coupon::find($id);
+        $instid = Auth::user()->id;
+        $courses = Course::where('instructor_id',$instid)->get();
+
+        return view('instructor.coupon.coupon_edit',compact('coupon','courses'));
+
+
+    } // End Method
+
+
+    public function InstructorUpdateCoupon(Request $request){
+
+        $coupon_id = $request->coupon_id;
+
+        Coupon::find($coupon_id)->update([
+            'coupon_name' => strtoupper($request->coupon_name),
+            'coupon_discount' => $request->coupon_discount,
+            'coupon_validity' => $request->coupon_validity,
+            'instructor_id' => Auth::user()->id,
+            'course_id' => $request->course_id,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'Coupon Updated Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->route('instructor.all.coupon')->with($notification);
+
+
+    } // End Method
+
+
+    public function InstructorDeleteCoupon($id){
+
+        Coupon::find($id)->delete();
+
+        $notification = array(
+            'message' => 'Coupon Deleted Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // End Method
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -256,6 +256,18 @@ Route::middleware(['auth','roles:instructor'])->group(function(){
         Route::post('/instructor/replay','InstructorReplay')->name('instructor.replay');
     });
 
+    // Instructor coupon All Route
+    Route::controller(CouponController::class)->group(function(){
+
+        Route::get('/instructor/all/coupon','InstructorAllCoupon')->name('instructor.all.coupon');
+        Route::get('/instructor/add/coupon','InstructorAddCoupon')->name('instructor.add.coupon');
+        Route::post('/instructor/store/coupon','InstructorStoreCoupon')->name('instructor.store.coupon');
+        Route::get('/instructor/edit/coupon/{id}','InstructorEditCoupon')->name('instructor.edit.coupon');
+        Route::post('/instructor/update/coupon','InstructorUpdateCoupon')->name('instructor.update.coupon');
+        Route::get('/instructor/delete/coupon/{id}','InstructorDeleteCoupon')->name('instructor.delete.coupon');
+
+    });
+
 
 }); // End Instructor Group Middleware
 
