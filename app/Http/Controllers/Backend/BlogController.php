ac0cfc0709db5c54c9698fe8557edce37c932805
Musfikur-93 <iradil713@gmail.com>
2024-05-22 16:35:07 +0600

    Paginations

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -248,7 +248,7 @@ class BlogController extends Controller
 
     public function BlogList(){
 
-        $blog = BlogPost::latest()->get();
+        $blog = BlogPost::latest()->paginate(2);
         $bcategory = BlogCategory::latest()->get();
         $post = BlogPost::latest()->limit(3)->get();
 
